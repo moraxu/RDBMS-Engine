f558d4a9a42c2386513bd91e946a8d7edf471c3a
Michal Guzek <mguzek@uci.edu>
2019-10-01 16:45:30 -0700

    Added few functions

--- rbf/rbfm.cc
+++ rbf/rbfm.cc
@@ -1,4 +1,9 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
 #include "rbfm.h"
+
+using namespace std;
 
 RecordBasedFileManager *RecordBasedFileManager::_rbf_manager = nullptr;
 
@@ -16,19 +21,19 @@ RecordBasedFileManager::RecordBasedFileManager(const RecordBasedFileManager &) =
 RecordBasedFileManager &RecordBasedFileManager::operator=(const RecordBasedFileManager &) = default;
 
 RC RecordBasedFileManager::createFile(const std::string &fileName) {
-    return -1;
+    return PagedFileManager::instance().createFile(fileName);
 }
 
 RC RecordBasedFileManager::destroyFile(const std::string &fileName) {
-    return -1;
+    return PagedFileManager::instance().destroyFile(fileName);
 }
 
 RC RecordBasedFileManager::openFile(const std::string &fileName, FileHandle &fileHandle) {
-    return -1;
+    return PagedFileManager::instance().openFile(fileName, fileHandle);
 }
 
 RC RecordBasedFileManager::closeFile(FileHandle &fileHandle) {
-    return -1;
+    return PagedFileManager::instance().closeFile(fileHandle);
 }
 
 RC RecordBasedFileManager::insertRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
@@ -47,7 +52,39 @@ RC RecordBasedFileManager::deleteRecord(FileHandle &fileHandle, const std::vecto
 }
 
 RC RecordBasedFileManager::printRecord(const std::vector<Attribute> &recordDescriptor, const void *data) {
-    return -1;
+    unsigned nullInfoFieldLength = static_cast<unsigned>(ceil(recordDescriptor.size()/8));
+    const byte* actualData = reinterpret_cast<const byte*>(data) + nullInfoFieldLength;
+
+    for(unsigned i = 0 ; i < recordDescriptor.size() ; ++i) {
+        cout << recordDescriptor[i].name << ": ";
+        const byte* byteInNullInfoField = reinterpret_cast<const byte*>(data) + i/8;
+        bool nullField = *byteInNullInfoField & (1 << 7-i%8);
+        if(nullField) {
+            cout << "NULL\t";
+        }
+        else {
+            if(recordDescriptor[i].type == AttrType::TypeInt) {
+                cout << *reinterpret_cast<const int*>(actualData) << "\t";
+                actualData += 4;
+            }
+            else if(recordDescriptor[i].type == AttrType::TypeReal) {
+                cout << *reinterpret_cast<const float*>(actualData) << "\t";
+                actualData += 4;
+            }
+            else { //recordDescriptor[i].type == AttrType::TypeVarChar
+                unsigned varCharLength = *reinterpret_cast<const unsigned*>(actualData);
+                actualData += 4;
+                unsigned char* varCharContent = new unsigned char[varCharLength+1];
+                varCharContent[varCharLength] = '\0';
+                memcpy(varCharContent, actualData, varCharLength);
+                cout << *varCharContent << "\t";
+                delete varCharContent;
+                actualData += varCharLength;
+            }
+        }
+    }
+
+    return 0;
 }
 
 RC RecordBasedFileManager::updateRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
